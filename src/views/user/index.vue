fe1033a6fa1bef3b54a77b1e8cf60857f63b9063
pjj <15705547960@163.com>
2021-02-08 10:26:55 +0800

    fi

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,79 +1,263 @@
 <template>
-  <div class="user">
-    <van-notice-bar color="#ff0000" background="#ecf9ff"  scrollable text="请正确填写信息，用于后期礼品的邮寄地址。" />
-    <div class="box">
-      <van-cell-group style="paddingLeft:10px">
-        <van-field class="rules" label="姓名" v-model="form.user_name" placeholder="请填写真实姓名" />
-        <van-field class="rules" label="地址" v-model="form.user_address" placeholder="请填写地址" />
-        <van-field class="rules" label="手机号" v-model="form.user_mobile" placeholder="请填写手机号" />
-        <van-field class="rules" label="微信号" v-model="form.user_WeChat" placeholder="请填写微信号" />
-        <!-- <van-field class="rules" label="姓名" v-model="name" placeholder="请填写真实姓名" /> -->
-      </van-cell-group>
+  <div class="login">
+    <!-- <van-icon name="arrow-left" size="20" color="#333" style="position: absolute; top: 10px; left: 10px;" @click="close()"/> -->
+    <div class="logo">
+      <!-- <img src="@/assets/img/login/login_bg_logo.png"> -->
+      <!-- <h2>随时随地&nbsp;&nbsp;轻松管理</h2> -->
     </div>
-    <div class="btn-sub">
-        <van-button round size="large" color="linear-gradient(to right, #79DABD, #4CD7EC)" type="button" native-type="submit" @click="subInfo">提交</van-button>
+    <div class="yun-01">
+      <img src="@/assets/img/login/login_bg_yun.png">
+    </div>
+    <div class="form">
+      <div class="phone cover">
+        <van-field
+          v-model="form.user_name"
+          :left-icon="phoneImg.showImg"
+          clearable
+          placeholder="请输入姓名"
+        />
       </div>
+      <div class="phone cover">
+        <van-field
+          v-model="form.user_mobile"
+          type="digit"
+          :left-icon="phoneImg.showImg"
+          clearable
+          placeholder="请输入手机号码"
+          @blur="phoneBlur"
+          @input="phoneInput"
+          @focus="phoneFocus"
+        />
+      </div>
+      <div class="number cover">
+        <van-field
+          :left-icon="authCodeImg.showImg"
+          v-model="authCode"
+          type="digit"
+          placeholder="请输入验证码"
+          @input="phoneInput"
+        >
+          <template #button>
+            <span v-if="countCodeFlag" >{{ codeInfo }}</span>
+            <span v-else class="count-code-flag">重新发送{{ countCode }}</span>
+          </template>
+        </van-field>
+      </div>
+    </div>
+    <div class="yun-02">
+      <img src="@/assets/img/login/login_bg_yun_b.png">
+      <span v-if="phoneFlag">请输入正确的手机号码</span>
+    </div>
+    <div class="sub-btn" >
+      <van-button @click="submitBtn" round size="large" color="linear-gradient(to right, #79DABD, #4CD7EC)" type="button">登录</van-button>
+    </div>
+    <!-- <div class="sub-btn" v-else>
+      <van-button round size="large" color="linear-gradient(to right, #BBEDE0, #A6EBF5)" type="button">登录</van-button>
+    </div> -->
+    <div class="footer">
+      <!-- <img src="@/assets/img/login/login_bg.png"/> -->
+    </div>
   </div>
 </template>
-
 <script>
 import { addUser,getStopUser } from '@/api'
 export default {
   data() {
     return {
+      account: '',
+      authCode: '',
       form: {
         stop_id: this.$route.query.stop_id,
         user_name: '',
         user_mobile: '',
-        user_address: '',
-        user_WeChat: '',
       },
-      name: '',
+      phoneFlag: false,
+      authCodeFlag: false,
+      redirect:'',
+      phoneImg:{
+        showImg: require('@/assets/img/login/login_input_icon_phone_normal.png'),
+        unfinished: require('@/assets/img/login/login_input_icon_phone_normal.png'),
+        finished: require('@/assets/img/login/login_input_icon_phone_pressed.png')
+      },
+      authCodeImg: {
+        showImg: require('@/assets/img/login/login_input_icon_verification_normal.png'),
+        unfinished: require('@/assets/img/login/login_input_icon_verification_normal.png'),
+        finished: require('@/assets/img/login/login_input_icon_verification_pressed.png')
+      },
+      schoolList: [],
+      schoolShow: false,
+      countCode:59,
+      countCodeFlag: true,
+      codeInfo:'获取验证码',
+      overlayShow: false,
     }
   },
   mounted() {
+    getStopUser().then(res => {
+      console.log(res)
+    })
   },
   methods: {
-    subInfo() {
-      addUser(this.form).then(res=> {
+    phoneBlur() {
+
+    },
+    phoneInput() {
+
+    },
+    phoneFocus() {
+
+    },
+    submitBtn() {
+      addUser(this.form).then(res => {
         this.$router.push({ name: 'succeed' })
       })
-    }
-  },
+    },
+  }
 }
 </script>
-
 <style lang="scss" scoped>
-.user {
-  width: 100%;
+.login{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 100%;
-  background: url('../../assets/img/shenqingdan_bg.png') no-repeat;
-  background-size: 100%;
-  background-color: #ffffff;
-  box-sizing: border-box;
-  .box {
-    margin: 20px;
-    margin-top: 50px;
+  .logo{
+    overflow: hidden;
+    margin-top: 52px;
+    img{
+      width: 134px;
+      height: 52px;
+    }
+    h2{
+      font-size: 12px;
+      margin-top: 5px;
+      color: #00DFC9;
+      text-align: center;
+      letter-spacing: 2px;
+    }
+  }
+  .yun-01{
+    overflow: hidden;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    img{
+      height: 30px;
+      width: 100px;
+    }
+  }
+  .yun-02{
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    margin-top: 8px;
+    img{
+      height: 30px;
+      width: 100px;
+    }
+    span{
+      position: absolute;
+      left: 60px;
+      font-size: 12px;
+      background: #FFEBEB;
+      display: inline-block;
+      padding:2px 15px;
+      border-radius: 15px;
+      color: #F44E4E;
+      line-height: 30px;
+      letter-spacing: 2px;
+    }
+  }
+  .form{
     display: flex;
     flex-direction: column;
-    padding: 15px 0 6px;
-    border-radius: 10px;
-    background-color: #ffffff;
-    box-shadow: 1px 1px 15px 0px rgba(221, 221, 221, 0.3);
+    align-items: center;
+    overflow: hidden;
+    .cover{
+      width: 305px;
+      height: 44px;
+      line-height: 24px;
+      background-color: #F6F6F6;
+      border-radius: 22px;
+      box-sizing: border-box;
+      margin-top: 20px;
+      padding: 10px 30px;
+      .van-cell {
+        background-color: transparent;
+        padding: 0;
+        color: #000;
+        font-size: 16px;
+        ::v-deep .van-cell__value {
+          input::-webkit-input-placeholder {
+            color: #CCC;
+            font-size: 16px;
+          }
+        }
+      }
+    }
+    .number{
+      span{
+        float: right;
+        border-left: 1px solid #C7CDCB;
+        padding-left: 15px;
+        color: #00DFC9;
+        font-size: 14px;
+      }
+    }
+    .count-code-flag{
+      color: #C7CDCB !important;
+    }
   }
-}
-::v-deep .van-field__control {
-  text-align: right;
-}
-.btn-sub {
-  margin-top: 30px;
-  padding: 20px 10px;
-  .van-button__text {
-    font-size: 18px;
-    line-height: 50px;
+  .sub-btn{
+      width: 90%;
+      margin: 20px auto;
+      button{
+        font-size: 16px;
+        font-weight: 500;
+      }
+    }
+  .footer{
+    width: 100%;
+    flex: 1;
+    // background: url('../../assets/img/login/login_bg.png') no-repeat bottom;
+    background-size: 100% 100px;
+    img{
+      width: 100%;
+      height: 100px;
+    }
   }
-  .van-button--large {
-    line-height: 50px;
+  .school-popup {
+    width: 100%;
+    height: 100vh;
+    padding-top: 46px;
+    ::v-deep .van-cell-group__title {
+      padding: 4px 16px;
+    }
+    .van-cell {
+      height: 60px;
+      ::v-deep .van-cell__left-icon {
+        img {
+          width: 40px;
+          height: 40px;
+          vertical-align: -4px;
+        }
+      }
+      ::v-deep .van-cell__title {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .title {
+          color: #333;
+          font-size: 14px;
+        }
+        .sub-title {
+          color: #333;
+          font-size: 10px;
+        }
+      }
+    }
   }
 }
 </style>
